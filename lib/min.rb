00fdc70951904cce49b86660dda3900284430a2e
Sarah Harrs <sarahharrs@gmail.com>
2019-07-10 01:25:23 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -8,5 +8,12 @@ groceries = {
 }
 
 def get_the_min(groceries)
-  groceries.flatten.min
+ letters = {a: 1, b: 2}
+ 
+letters.min
+#  => [:a, 1]
+ 
+alphabetical_order = {c: 100, d: 1}
+alphabetical_order.min
+groceries.values.flatten.min
 end
